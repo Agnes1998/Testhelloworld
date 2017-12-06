b3ed846b6e4c362bfb5b3bcd367dd3d658b24d9a
Agnes1998 <34218424+Agnes1998@users.noreply.github.com>
2017-12-05 21:54:54 -0600

    working some formatted

--- src/Testhelloworld/Stacksorter.java
+++ src/Testhelloworld/Stacksorter.java
@@ -41,9 +41,9 @@ public class Stacksorter {
 	    
 	    
 	    
-	    System.out.println( " first element Set  ******************************************************** " + maxCandidate);	  
-	    System.out.println( "number of elements in stack " + i );
-	    System.out.println( "First round Our counter count " + counter + "  has max candidate "  + maxCandidate);
+	    													//System.out.println( " first element Set  ******************************************************** " + maxCandidate);	  
+	    													//System.out.println( "number of elements in stack " + i );
+	    													//System.out.println( "First round Our counter count " + counter + "  has max candidate "  + maxCandidate);
 	  	
 	    
 	    
@@ -52,43 +52,42 @@ public class Stacksorter {
 	     counter++;	
 	     
 	     
-	     System.out.println( "Our counter count " + counter + " ------------- has max candidate "  + maxCandidate + " number on stack i = " + i);
+	     													//System.out.println( "Our counter count " + counter + " ------------- has max candidate "  + maxCandidate + " number on stack i = " + i);
 	     
 	     
 	     //put max on bottom
 	     st.push(maxCandidate);	
 	     
 	     
-	     System.out.println( ">>>>>>>>>>>>>>>>>>>>>MaxCandidate  " + maxCandidate + " put on stack ----- counter " + counter);
+	     													//System.out.println( ">>>>>>>>>>>>>>>>>>>>>MaxCandidate  " + maxCandidate + " put on stack ----- counter " + counter);
 	 
 	     moveStack2backToStack1( st2, st );
 	 	
 	     
 	    maxCandidate = st.pop();
-	    System.out.println( "Top of stack reset  to new MaxCandidate  " + maxCandidate + " counter " + counter);
+	    													//System.out.println( "Top of stack reset  to new MaxCandidate  " + maxCandidate + " counter " + counter);
 	    //now we know the number of elements in st is i
 	    for (int k = 1 ; k < i-counter  ; k++)
 	    {
-	    	System.out.println( " ~~~~~~~~~~  Iteration " +  k + " my MaxCandidate  " + maxCandidate + " counter " + counter);
-	    	System.out.println( " ~~~~~~~~~~  considering " + st.peek());
+	    													//System.out.println( " ~~~~~~~~~~  Iteration " +  k + " my MaxCandidate  " + maxCandidate + " counter " + counter);
+	    													//System.out.println( " ~~~~~~~~~~  considering " + st.peek());
             if ( maxCandidate > st.peek())
             {
-            	System.out.println( " ============  Iteration " +  k + " my element moved to other pile  " + st.peek());
+            												//System.out.println( " ============  Iteration " +  k + " my element moved to other pile  " + st.peek());
             	st2.push(st.pop());  //disregard candidate put  to other pile
             }
             else
             {
-            	st2.push(maxCandidate); //changed max candidate to new element and put old max in another pile
+                st2.push(maxCandidate); //changed max candidate to new element and put old max in another pile
             	maxCandidate = st.pop();
-            	System.out.println( " ================= Iteration " +  k + " my MaxCandidate changed " + maxCandidate + " counter " + counter);
+            												//System.out.println( " ================= Iteration " +  k + " my MaxCandidate changed " + maxCandidate + " counter " + counter);
             }	
 	    }
-	    //
-	    
-	    
+	   
+
 	    }
 	
-	    st.push(maxCandidate);	
+	   st.push(maxCandidate);	
 	   return st; 
 	   
 	}
@@ -96,13 +95,13 @@ public class Stacksorter {
 	
 	void moveStack2backToStack1(Stack<Integer> source,Stack <Integer> destination )
 	{
-		int j = 0;
+														//int j = 0;
 		while( !source.isEmpty())
-	    {  j++;
-		 System.out.println( "Popping  element " + j + "  "  + source.peek()  );
+	    {                                               //j++;
+		 												//System.out.println( "Popping  element " + j + "  "  + source.peek()  );
 		 destination.push(source.pop());
 		
 	    }
-		 System.out.println( "-------------------------------------------Moved " + (j) + "  elements"  );
+		 												//System.out.println( "-------------------------------------------Moved " + (j) + "  elements"  );
 	}
 }
